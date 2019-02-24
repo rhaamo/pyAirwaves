213b727e453696ef47cc8354b3f07c61cb4379d5
Dashie <dashie@sigpipe.me>
2019-02-24 15:06:04 +0100

    Add CI and requirements

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+setup(
+    name="pyAirwaves",
+    version="0.1",
+    license="AGPL3",
+    python_requires=">=3.6",
+    long_description=open("README.md").read(),
+    url="https://github.com/rhaamo/pyAirwaves",
+    author="Dashie",
+    author_email="dashie@sigpipe.me",
+    install_requires=[
+        "Flask",
+        "SQLAlchemy",
+        "SQLAlchemy-Searchable",
+        "SQLAlchemy-Utils",
+        "SQLAlchemy-Continuum",
+        "Bootstrap-Flask",
+        "Flask-DebugToolbar",
+        "Flask-Mail",
+        "Flask-Migrate",
+        "Flask-SQLAlchemy",
+        "bcrypt",
+        "psycopg2-binary",
+        "unidecode",
+        "Flask_Babelex",
+        "texttable",
+        "python-slugify",
+        "python-magic",
+        "redis",
+        "flask-socketio"
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest", "pytest-cov", "jsonschema"],
+)
