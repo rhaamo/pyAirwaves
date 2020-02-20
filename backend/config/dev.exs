95b66faa05cd19cfe450025612fd854f96a9bf51
Dashie <dashie@sigpipe.me>
2020-02-20 10:19:02 +0100

    Remove node watcher

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -21,13 +21,6 @@ config :pyairwaves, PyairwavesWeb.Endpoint,
   code_reloader: true,
   check_origin: false,
   watchers: [
-    node: [
-      "node_modules/webpack/bin/webpack.js",
-      "--mode",
-      "development",
-      "--watch-stdin",
-      cd: Path.expand("../assets", __DIR__)
-    ]
   ]
 
 # ## SSL Support
