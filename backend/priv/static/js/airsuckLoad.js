529d03cb69e805b3647eaa45a5940102e5ac4016
Dashie <dashie@sigpipe.me>
2020-02-20 10:37:18 +0100

    Import front css/js in app

---
+++ backend/priv/static/js/airsuckLoad.js
@@ -0,0 +1,125 @@
+"use strict"; // overcome current Chrome and Firefox issues with ECMA6 stuff like classes
+/***********************************************************
+ * Airsuck JS initiator
+ * v. 0.1
+ *
+ * Licensed under GPL V3
+ * https://github.com/ThreeSixes/airSuck
+ *
+ * Load required and optional JS files,
+ * init JS main functions
+ *
+ * Deps: jquery
+ **********************************************************/
+
+/* exported vehicleTypes, vehicles */
+/* global debug, socket, mapLoaded, vehExpireCheckInterval, expireVehicles */
+
+/***************************************************
+ * GLOBALS
+ **************************************************/
+var sidebarLoaded = false;
+var vehicleTypes = []; // Array for registering vehicle types (AIS, ADSB)
+var vehicles = []; // Main array holding vehicles - replacing vehData array with vehicle objects
+
+/***************************************************
+ * LOAD CONFIG OPTIONS AND HELPER SCRIPTS
+ **************************************************/
+$.getScript("/js/config.js")
+    .done(function () {
+        if (debug) {
+            $('.msgBx').removeClass('dbgInactive').addClass('dbgActive');
+        }
+        // set the html object ID for sending on-screen debug and messages
+        window.messageBx = 'message';
+
+        // Initialize the logger
+        Logger.useDefaults()
+        if (debug) {
+            Logger.setLevel(Logger.trace)
+        } else {
+            Logger.setLevel(Logger.error)
+        }
+        Logger.info('Configuration loaded.')
+    })
+    .fail(function (jqxhr, settings, exception) {
+        console.log("ERROR: Cannot load configuration file", exception)
+    });
+
+
+// Initiate the WebSocket
+window.socket = new WebSocket('ws://' + document.domain + ':' + location.port + '/ws');
+
+function socketOk(msg) {
+    $("#websocket-status").removeClass().addClass(["badge badge-pill", "badge-success"]).prop('title', msg);
+    $("#websocket-status i").removeClass().addClass(["fa", "fa-check icon-socket-ok"]);
+    Logger.info("SocketIO OK: " + msg);
+}
+
+function socketNok(msg) {
+    $("#websocket-status").removeClass().addClass(["badge badge-pill", "badge-success"]).prop('title', msg);
+    $("#websocket-status i").removeClass().addClass(["fa", "fa-close icon-socket-nok"]);
+    Logger.info("SocketIO Err: " + msg);
+
+}
+
+socket.onopen = function(event) {
+    socketOk("Websocket is connected");
+};
+
+socket.onclose = function(event) {
+    socketNok("Websocket error", event);
+};
+
+socket.onerror = function(event) {
+    socketNok("Websocket error", event);
+};
+
+/***************************************************
+ * SETUP AND LOAD VEHICLES
+ **************************************************/
+// load Vehicles early as the types are prerequisite to other files
+// load the Vehicle class first
+$.getScript("/js/vehicles/vehicle.js", function () {
+    // Start the vehcile expiration routine which should be executed at the specified interval.
+    window.setInterval(function () {
+        // If our map has been loaded then we can start "expiring" vehicles.
+        if (mapLoaded) {
+            expireVehicles();
+        }
+    }, vehExpireCheckInterval);
+    // Load any custom vehicles
+    let index;
+    for (index = 0; index < loadCustomVehicles.length; index++) {
+        Logger.info('Loading custom vehicle: ' + loadCustomVehicles[index]);
+        $.getScript("/js/vehicles/" + loadCustomVehicles[index]);
+    }
+
+    // Initialize the sidebar
+    window.sidebar = L.control.sidebar('sidebar', {position: 'right'}).addTo(map);
+
+    // Load the message handler
+    $.getScript("/js/core/messages.js");
+});
+
+/***************************************************
+ * SETUP AND LOAD MAPS
+ **************************************************/
+$(document).ready(function () {
+    // Load RainbowVis to color vehicle paths by height
+    $.getScript("/js/plugins/rainbowvis.js", function () {
+        // Instanciate RainbowVis and set global color ramp by plane height
+        window.polyRamp = new Rainbow();
+        polyRamp.setNumberRange(minimumAltitude, maximumAltitude);
+        polyRamp.setSpectrum(spectrum[0], spectrum[1], spectrum[2], spectrum[3]);
+    });
+    // Globals
+    window.map = null;
+    window.layers = null;
+    window.mapLoaded = false;
+    window.vehData = {}; // Create a generic array to hold our vehicle data
+    $.getScript("/js/core/map_init.js", function () {
+        initMap();
+        Logger.info("Map initialized.")
+    });
+});
---
+++ backend/priv/static/js/core/messages.js
@@ -0,0 +1,86 @@
+"use strict"; // overcome current Chrome and Firefox issues with ECMA6 stuff like classes
+/***********************************************************
+ * Airsuck JS Socket Message Handler
+ * v. 0.1
+ *
+ * Licensed under GPL V3
+ * https://github.com/ThreeSixes/airSuck
+ *
+ * Handles socket.io messages for vehicle data coming from node
+ *
+ * Deps: jQuery, vehicles.js, leaflet maps initialized
+ **********************************************************/
+
+/***************************************************
+ * SOCKET MESSAGE HANDLER
+ **************************************************/
+
+function handleMessage(event) {
+    const msgJSON = JSON.parse(event.data);
+    if (!mapLoaded) {
+        // Return early - the map isn't loaded yet
+        Logger.error("Maps not loaded. Discarding aircraft data.");
+        return;
+    }
+    // Dump the JSON string to the message box.
+    if (debug) {
+        $('#' + messageBx).attr('value', JSON.stringify(msgJSON, null, 2));
+    }
+
+    // Return early - don't continue processing the keepalive.
+    if ('keepalive' in msgJSON) {
+        return;
+    }
+
+    // Store the vehicle name for reference in the function
+    let vehName = "veh" + msgJSON.addr.toString();
+
+    // See if we have a vehicle in our vehicle data.
+    if (vehName in vehicles && vehicles[vehName] != null) {
+        // existing vehicle, call the update function
+        vehicles[vehName].update(msgJSON);
+    } else {
+        // new vehicle, call the constructor, create marker and set listeners
+        let index; // we need an index to manually traverse the vehicleTypes associative array
+        let length = vehicleTypes.length;
+        for (index = 0; index < length; ++index) {
+            if (msgJSON.type === vehicleTypes[index].protocol) {
+                // add the new vehicle (constructor should call registered update functions)
+                vehicles[vehName] = vehicleTypes[index].constructor(msgJSON);
+
+                // Add marker and listeners only if we have a Latitude and Longitude
+                // Cannot create a marker without geoposition datas
+                if (vehicles[vehName].lat && vehicles[vehName].lon) {
+                    const markerBounds = L.latLngBounds(L.latLng(msgJSON.lat, msgJSON.lon), L.latLng(msgJSON.lat, msgJSON.lon));
+                    if (!map.getBounds().contains(markerBounds)) {
+                        // Do something if out-of-bounds
+                        // Ideally we would drop the message in the future
+                    }
+                    // create a marker icon for the vehicle (may move to the constructor)
+                    vehicles[vehName].setMarker();
+
+                    // Add listeners to marker - must be here to access the vehicles array
+                    vehicles[vehName].marker.on('click', function () {
+                        vehicleMarkerClickListener(vehName);
+                    });
+                    vehicles[vehName].marker.on('contextmenu', function () {
+                        vehicleMarkerRightClickListener(vehName);
+                        return false; // cancel browser context menu popup
+                    });
+                } else {
+                    //debugger;
+                    Logger.warn('handleMessage: No Lat/Lon available for: ' + vehicles[vehName].addr);
+                }
+                break;
+            } else if (index === length) {
+                // vehicle type not registered, drop data
+                Logger.error('New vehicle found, type not registered: ' + msgJSON.type + ' Dropping data.')
+            }
+        }
+    }
+}
+
+// Register the message handler
+socket.onmessage = function (event) {
+    handleMessage(event);
+}
