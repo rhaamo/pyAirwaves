2f7a405a0cc29821cd2389cc560d72c7a7417424
Dashie <dashie@sigpipe.me>
2020-02-26 16:32:37 +0100

    Fix query

--- backend/lib/pyairwaves_web/controllers/archive_controller.ex
+++ backend/lib/pyairwaves_web/controllers/archive_controller.ex
@@ -5,7 +5,7 @@ defmodule PyairwavesWeb.ArchiveController do
   def ais_quick(conn, _params) do
     sources =
       Pyairwaves.ArchiveSource
-      |> Ecto.Query.where([type: "airAIS"])
+      |> Ecto.Query.where(type: "airAIS")
       |> Ecto.Query.order_by([a], asc: a.inserted_at)
       |> Pyairwaves.Repo.all()
 
@@ -30,22 +30,26 @@ defmodule PyairwavesWeb.ArchiveController do
   def adsb_quick(conn, _params) do
     sources =
       Pyairwaves.ArchiveSource
-      |> Ecto.Query.where([type: "airADSB"])
+      |> Ecto.Query.where(type: "airADSB")
       |> Ecto.Query.order_by([a], asc: a.inserted_at)
       |> Pyairwaves.Repo.all()
 
-      q_aircrafts =
-        Ecto.Query.from(aa in Pyairwaves.ArchiveAircraft,
-          left_join: am in Pyairwaves.AircraftMode,
-          left_join: a in Pyairwaves.Aircraft,
-          on: am.mode_s == aa.hex_ident,
-          on: am.icao_type_code == a.icao,
-          select: %{mode_s: am.mode_s, mode_s_country: am.mode_s_country, description: a.aircraft_description},
-          limit: 15,
-          order_by: [asc: aa.inserted_at]
-        )
+    q_aircrafts =
+      Ecto.Query.from(aa in Pyairwaves.ArchiveAircraft,
+        left_join: am in Pyairwaves.AircraftMode,
+        on: am.mode_s == aa.hex_ident,
+        left_join: a in Pyairwaves.Aircraft,
+        on: am.icao_type_code == a.icao,
+        select: %{
+          mode_s: am.mode_s,
+          mode_s_country: am.mode_s_country,
+          description: a.aircraft_description
+        },
+        limit: 15,
+        order_by: [asc: aa.inserted_at]
+      )
 
-      aircrafts = Pyairwaves.Repo.all(q_aircrafts, timeout: :infinity)
+    aircrafts = Pyairwaves.Repo.all(q_aircrafts, timeout: :infinity)
 
     render(conn, "adsb_quick.html",
       version: Pyairwaves.Application.version(),
@@ -53,9 +57,12 @@ defmodule PyairwavesWeb.ArchiveController do
       sources: sources,
       aircrafts: aircrafts,
       count_sources: Enum.count(sources),
-      count_aircrafts: Pyairwaves.Repo.one(Ecto.Query.from(a in Pyairwaves.ArchiveAircraft, select: count(a.id))),
+      count_aircrafts:
+        Pyairwaves.Repo.one(Ecto.Query.from(a in Pyairwaves.ArchiveAircraft, select: count(a.id))),
       count_messages:
-        Pyairwaves.Repo.one(Ecto.Query.from(a in Pyairwaves.ArchiveAircraftMessage, select: count(a.id)))
+        Pyairwaves.Repo.one(
+          Ecto.Query.from(a in Pyairwaves.ArchiveAircraftMessage, select: count(a.id))
+        )
     )
   end
 end
