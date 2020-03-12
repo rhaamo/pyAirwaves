ec31d52dde45976752e3fd6cc0c6187519009cb6
Dashie <dashie@sigpipe.me>
2020-03-12 07:55:58 +0100

    Add Credo

--- backend/mix.exs
+++ backend/mix.exs
@@ -57,7 +57,8 @@ defmodule Pyairwaves.MixProject do
       # geocalc 0.8.0 is currently borked, see https://github.com/yltsrc/geocalc/issues/55
       {:geocalc, git: "https://github.com/yltsrc/geocalc", ref: "master"},
       {:math, "~> 0.5.0"},
-      {:ais, path: "../../forks/ais"}
+      {:ais, path: "../../forks/ais"},
+      {:credo, "~> 1.2", only: [:dev, :test], runtime: false}
     ]
   end
 
