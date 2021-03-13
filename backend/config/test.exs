5bc874188479c4a51619b5569f9453645c491dd7
Dashie <dashie@sigpipe.me>
2021-03-13 09:20:47 +0100

    use right postgres infos in testing

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -5,7 +5,7 @@ config :pyairwaves, Pyairwaves.Repo,
   username: "postgres",
   password: "postgres",
   database: "pyairwaves_test",
-  hostname: "database",
+  hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
